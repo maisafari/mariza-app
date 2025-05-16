6f2844da8ef126c1ae176165323bf5d20ceeaba7
Mariza Buntubwimana <marizabuntubwimana@gmail.com>
2025-05-17 00:46:10 +0100

    add blog page redo

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,8 @@
 export default function BlogPage() {
   return (
-    <div>
-      <h1>Blog</h1>
-      <p>Welcome to my blog!</p>
+    <div className="p-8">
+      <h1 className="text-3xl font-bold">Blog</h1>
+      <p className="mt-4 text-gray-600">Welcome to the blog section. Posts coming soon!</p>
     </div>
   );
 }
